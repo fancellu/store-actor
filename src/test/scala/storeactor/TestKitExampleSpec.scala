18a7942d70a055ccd4b454128d39ee7f2a2bb7de
Dino Fancellu <dino.fancellu.freelancer@guardian.co.uk>
2019-07-22 12:15:27 +0100

    Adding EventFilter example to test Actor log, useful

--- src/test/scala/storeactor/TestKitExampleSpec.scala
+++ src/test/scala/storeactor/TestKitExampleSpec.scala
@@ -1,13 +1,17 @@
 package storeactor
 
 import akka.actor.{ActorSystem, Props}
-import akka.testkit.{ImplicitSender, TestKit}
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
 import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
 import storeactor.StoreActor.{ADDITEM, GETALL}
 
 import scala.concurrent.duration._
 
-class TestKitExampleSpec extends TestKit(ActorSystem("ActorSystemTK"))
+class TestKitExampleSpec extends TestKit(ActorSystem("ActorSystemTK", ConfigFactory.parseString(
+  """
+    akka.loggers = ["akka.testkit.TestEventListener"]
+    """)))
   with ImplicitSender with WordSpecLike with BeforeAndAfterAll with Matchers {
 
   override def afterAll(): Unit = {
@@ -16,29 +20,32 @@ class TestKitExampleSpec extends TestKit(ActorSystem("ActorSystemTK"))
 
   "storeactor" should {
     "reply with items" in {
-      val store = system.actorOf(Props(new StoreActor[Int]),"StoreActorTK1")
+      val store = system.actorOf(Props(new StoreActor[Int]), "StoreActorTK1")
 
       store ! ADDITEM(1)
       store ! ADDITEM(2)
       store ! GETALL
 
-      expectMsg(Vector(1,2))
+      expectMsg(Vector(1, 2))
 
       store ! ADDITEM(3)
 
       store ! GETALL
 
-      val all=expectMsgType[Vector[Int]]
+      val all = expectMsgType[Vector[Int]]
 
-      all should be(Vector(1,2,3))
+      all should be(Vector(1, 2, 3))
 
       system.stop(store)
     }
 
     "ignore rubbish" in {
-      val store = system.actorOf(Props(new StoreActor[Int]),"StoreActorTK2")
+      val store = system.actorOf(Props(new StoreActor[Int]), "StoreActorTK2")
 
-      store ! "ignoreme"
+      // Checking the log for appropriate warning
+      EventFilter.warning(pattern = "Was not expecting ignoreme", occurrences = 1).intercept {
+        store ! "ignoreme"
+      }
 
       expectNoMessage(0.5.second)
 
